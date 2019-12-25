2d0d85832813667b2b189bb02c1b16087c492d89
kajetankapela <kajetan.kapela@gmial.com>
2019-12-25 19:07:14 +0100

    stworzenie projektu

---
+++ src/main/java/com/gdziepotanczymy/GdziePotanczymyApplication.java
@@ -0,0 +1,13 @@
+package com.gdziepotanczymy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdziePotanczymyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdziePotanczymyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdziepotanczymy/GdziePotanczymyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdziepotanczymy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdziePotanczymyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
